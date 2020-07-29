6394f816e3738d7a3cf697f1ef44d896e44f4010
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2020-07-29 09:01:19 +0200

    ini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { TranslateService } from '@ngx-translate/core';
+import { first, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'i18n-showcase';
+export class AppComponent implements OnInit {
+  form = new FormGroup({});
+  model = {};
+  fields: FormlyFieldConfig[] = null;
+  public constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.translate
+      .use('de')
+      .pipe(
+        first(),
+        tap(
+          () =>
+            (this.fields = [
+              {
+                key: 'firstname',
+                type: 'input',
+                templateOptions: {
+                  label: this.translate.instant('FIRSTNAME'),
+                },
+              },
+              {
+                key: 'lastname',
+                type: 'input',
+                templateOptions: {
+                  label: this.translate.instant('LASTNAME'),
+                },
+              },
+            ])
+        )
+      )
+      .subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyBootstrapModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
